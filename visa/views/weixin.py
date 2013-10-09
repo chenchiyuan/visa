bcd41897701fef02d3cbb979a945c9d0a2059af4
chenchiyuan <chenchiyuan03@gmail.com>
2013-10-09 21:35:01 +0800

    fix bug

--- visa/views/weixin.py
+++ visa/views/weixin.py
@@ -2,6 +2,7 @@
 # __author__ = chenchiyuan
 
 from __future__ import division, unicode_literals, print_function
+from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import View
 from utils.weixin.receiver import WeiXinReceiver
@@ -12,7 +13,10 @@ class WeiXinInterfaceView(View):
         receiver = WeiXinReceiver(request)
         return HttpResponse(receiver.echo())
 
-    @csrf_exempt
     def post(self, request, *args, **kwargs):
         receiver = WeiXinReceiver(request)
         return HttpResponse(receiver.dispatch())
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super(WeiXinInterfaceView, self).dispatch(request, *args, **kwargs)
